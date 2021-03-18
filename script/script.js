44748db116e5e3bfd973ff823f72521d05e95efd
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2021-03-18 19:02:35 -0300

    Redimensionando para telas maiores

--- script/script.js
+++ script/script.js
@@ -1,12 +1,15 @@
+tamanhoTela = window.innerWidth
+
 background = document.querySelectorAll('#bg')
 bg = 0
 tempoSlide = 3000
+
 function slide() {
 
     setInterval(() => {
         background[bg].classList.add('fade-out')
         bg++
-        if (bg == 4) {
+        if (bg == background.length) {
             bg = 0
             for (i = 0; i < background.length; i++) {
                 background[i].classList.remove('fade-out')
@@ -15,6 +18,19 @@ function slide() {
     }, tempoSlide)
 }
 slide()
+
+async function mudarImagem() {
+    if (tamanhoTela >= 700) {
+        document.querySelector('.bg1').setAttribute('src', 'img/carousel01-G.jpg');
+        document.querySelector('.bg2').setAttribute('src', 'img/carousel02-G.jpg');
+        document.querySelector('.bg3').setAttribute('src', 'img/carousel03-G.jpg');
+        document.querySelector('.bg4').setAttribute('src', 'img/carousel04-G.jpg');
+    }else{
+        return false
+    }
+}
+
+mudarImagem()
 
 
 
