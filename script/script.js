f7a366d19884b69ec71c374d18f1a5c174fdc206
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2021-04-16 16:12:13 -0300

    tamanho da fonte de carregamento

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ function carregado() {
         document.querySelector('.carregando').style.display = 'none'
         slide()
         mudarImagem()
-    }, 2000)
+    }, 3000)
 }
 
 if (document.readyState != "complete") {
