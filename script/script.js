b32d7308684543a1998338fd58cc0d67ebbb65db
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2021-04-17 17:24:11 -0300

    mudança das imagens

--- script/script.js
+++ script/script.js
@@ -13,18 +13,18 @@ document.onreadystatechange = function () {
 
 tamanhoTela = window.innerWidth
 
-background = document.querySelectorAll('.bg')
+carousel = document.querySelectorAll('.bg')
 bg = 0
-tempoSlide = 4000
+tempoSlide = 3000
 
 function slide() {
     setInterval(() => {
-        background[bg].classList.add('fade-out')
+        carousel[bg].classList.add('fade-out')
         bg++
-        if (bg == background.length) {
+        if (bg == carousel.length) {
             bg = 0
-            for (i = 0; i < background.length; i++) {
-                background[i].classList.remove('fade-out')
+            for (i = 0; i < carousel.length; i++) {
+                carousel[i].classList.remove('fade-out')
             }
         }
     }, tempoSlide)
@@ -32,7 +32,7 @@ function slide() {
 
 function mudarImagem() {
     if (tamanhoTela >= 700) {
-        background.style = 'display: none;'
+        carousel.style = 'display: none;'
         document.querySelector('#bg1').setAttribute('src', 'img/carousel01-G.jpg');
         document.querySelector('#bg2').setAttribute('src', 'img/carousel02-G.jpg');
         document.querySelector('#bg3').setAttribute('src', 'img/carousel03-G.jpg');
