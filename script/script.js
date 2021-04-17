3658f16e78822d7ce35bec74b7573a4cd42432af
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2021-04-17 15:56:43 -0300

    ajustes finos no tempo de carregamento

--- script/script.js
+++ script/script.js
@@ -1,38 +1,5 @@
 document.onreadystatechange = function () {
     if (document.readyState === "complete") {
-        tamanhoTela = window.innerWidth
-
-        background = document.querySelectorAll('.bg')
-        bg = 0
-        tempoSlide = 3000
-
-        function slide() {
-            setInterval(() => {
-                background[bg].classList.add('fade-out')
-                bg++
-                if (bg == background.length) {
-                    bg = 0
-                    for (i = 0; i < background.length; i++) {
-                        background[i].classList.remove('fade-out')
-                    }
-                }
-            }, tempoSlide)
-        }
-
-        function mudarImagem() {
-            if (tamanhoTela >= 700) {
-                background.style = 'display: none;'
-                document.querySelector('#bg1').setAttribute('src', 'img/carousel01-G.jpg');
-                document.querySelector('#bg2').setAttribute('src', 'img/carousel02-G.jpg');
-                document.querySelector('#bg3').setAttribute('src', 'img/carousel03-G.jpg');
-                document.querySelector('#bg4').setAttribute('src', 'img/carousel04-G.jpg');
-            } else {
-                document.querySelector('#bg1').setAttribute('src', 'img/carousel01-P.jpg');
-                document.querySelector('#bg2').setAttribute('src', 'img/carousel02-P.jpg');
-                document.querySelector('#bg3').setAttribute('src', 'img/carousel03-P.jpg');
-                document.querySelector('#bg4').setAttribute('src', 'img/carousel04-P.jpg');
-            }
-        }
         slide()
         mudarImagem()
         document.querySelector('.carregando').style.display = 'none'
@@ -44,8 +11,39 @@ document.onreadystatechange = function () {
     }
 }
 
+tamanhoTela = window.innerWidth
 
+background = document.querySelectorAll('.bg')
+bg = 0
+tempoSlide = 4000
 
+function slide() {
+    setInterval(() => {
+        background[bg].classList.add('fade-out')
+        bg++
+        if (bg == background.length) {
+            bg = 0
+            for (i = 0; i < background.length; i++) {
+                background[i].classList.remove('fade-out')
+            }
+        }
+    }, tempoSlide)
+}
+
+function mudarImagem() {
+    if (tamanhoTela >= 700) {
+        background.style = 'display: none;'
+        document.querySelector('#bg1').setAttribute('src', 'img/carousel01-G.jpg');
+        document.querySelector('#bg2').setAttribute('src', 'img/carousel02-G.jpg');
+        document.querySelector('#bg3').setAttribute('src', 'img/carousel03-G.jpg');
+        document.querySelector('#bg4').setAttribute('src', 'img/carousel04-G.jpg');
+    } else {
+        document.querySelector('#bg1').setAttribute('src', 'img/carousel01-P.jpg');
+        document.querySelector('#bg2').setAttribute('src', 'img/carousel02-P.jpg');
+        document.querySelector('#bg3').setAttribute('src', 'img/carousel03-P.jpg');
+        document.querySelector('#bg4').setAttribute('src', 'img/carousel04-P.jpg');
+    }
+}
 
 
 
